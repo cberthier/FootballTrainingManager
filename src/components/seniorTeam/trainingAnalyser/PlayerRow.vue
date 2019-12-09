07e732729748beb530a8922df2bef3049883fdb3
Cyril Berthier <berthier.cyril.fr@gmail.com>
2019-12-09 16:51:36 +0100

    Add dialog to update training week

--- src/components/seniorTeam/trainingAnalyser/PlayerRow.vue
+++ src/components/seniorTeam/trainingAnalyser/PlayerRow.vue
@@ -12,43 +12,43 @@
       <v-chip-skill :value="player.keeper" :oldValue="player.oldKeeper"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'Keeper'">
-      <v-chip-training :value="player.nextLevelKeeper"/>
+      <v-chip-training :player="player" skill="Keeper" :value="player.nextLevelKeeper"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'None' || trainingSelected == 'Defending'">
       <v-chip-skill :value="player.defending" :oldValue="player.oldDefending"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'Defending'">
-      <v-chip-training :value="player.nextLevelDefending"/>
+      <v-chip-training :player="player" skill="Defending" :value="player.nextLevelDefending"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'None' || trainingSelected == 'Playmaking'">
       <v-chip-skill :value="player.playmaking" :oldValue="player.oldPlaymaking"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'Playmaking'">
-      <v-chip-training :value="player.nextLevelPlaymaking"/>
+      <v-chip-training :player="player" skill="Playmaking" :value="player.nextLevelPlaymaking"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'None' || trainingSelected == 'Winger'">
       <v-chip-skill :value="player.winger" :oldValue="player.oldWinger"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'Winger'">
-      <v-chip-training :value="player.nextLevelWinger"/>
+      <v-chip-training :player="player" skill="Winger" :value="player.nextLevelWinger"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'None' || trainingSelected == 'Passing'">
       <v-chip-skill :value="player.passing" :oldValue="player.oldPassing"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'Passing'">
-      <v-chip-training :value="player.nextLevelPassing"/>
+      <v-chip-training :player="player" skill="Passing" :value="player.nextLevelPassing"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'None' || trainingSelected == 'Scoring'">
       <v-chip-skill :value="player.scoring" :oldValue="player.oldScoring"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'Scoring'">
-      <v-chip-training :value="player.nextLevelScoring"/>
+      <v-chip-training :player="player" skill="Scoring" :value="player.nextLevelScoring"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'None' || trainingSelected == 'FreeKick'">
       <v-chip-skill :value="player.freeKick" :oldValue="player.oldFreeKick"/>
     </td>
     <td class="text-center" v-if="trainingSelected == 'FreeKick'">
-      <v-chip-training :value="player.nextLevelFreeKick"/>
+      <v-chip-training :player="player" skill="FreeKick" :value="player.nextLevelFreeKick"/>
     </td>
   </tr>
 </template>
--- src/components/seniorTeam/trainingAnalyser/VChipTraining.vue
+++ src/components/seniorTeam/trainingAnalyser/VChipTraining.vue
@@ -1,12 +1,46 @@
 <template>
-  <v-chip v-if="value != null" label class="ml-2" :color="color()">{{value}}w</v-chip>
-  <v-chip v-else label outlined class="ml-2">?</v-chip>
+  <v-container>
+    <!-- <v-btn color="primary" dark @click.stop="dialog = true">Open Dialog</v-btn> -->
+    <v-chip
+      v-if="value != null"
+      label
+      class="ml-2"
+      :color="color()"
+      @click.stop="dialog = true"
+    >{{value}}w</v-chip>
+    <v-chip v-else label outlined class="ml-2" @click.stop="dialog = true">?</v-chip>
+    <v-dialog v-model="dialog" max-width="290">
+      <v-card>
+        <v-card-title class="headline">{{player.firstName}} {{player.lastName}}</v-card-title>
+
+        <v-card-text>
+          <v-text-field
+            :label="skill"
+            name="value"
+            v-model="value"
+            type="number"
+            suffix="Week(s)"
+            required
+            :rules="numberPositifRules"
+          />
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn color="green darken-1" text @click="dialog = false">Cancel</v-btn>
+
+          <v-btn color="green darken-1" text @click="dialog = false">Submit</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "VChipTraining",
-  props: ["value"],
+  props: ["player", "skill", "value"],
   methods: {
     color() {
       if (this.value === 1) {
@@ -20,7 +54,13 @@ export default {
     }
   },
   data: function() {
-    return {};
+    return {
+      numberPositifRules: [
+        value => !!value || "This field is required",
+        value => value > 0 || "This field must be greater than 0"
+      ],
+      dialog: false
+    };
   }
 };
 </script>
